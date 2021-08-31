6b3bb317870503a781ccf5db0ced03893c95ef63
unknown <paulo.seph@hotmail.com>
2021-08-31 15:36:46 -0300

    Definindo estruturas inicias para o Produto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { CriarProdutoComponent } from './components/produto/criar-produto/criar-produto.component';
 
 
 
@@ -13,12 +14,12 @@ import { HomeComponent } from './components/home/home.component';
   declarations: [
     AppComponent,
     HomeComponent,
+    CriarProdutoComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MainLayoutModule,
-
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/produto/produto-routing.module.ts
+++ src/app/modules/produto/produto-routing.module.ts
@@ -1,9 +1,11 @@
 import { ProdutoComponent } from './../../components/produto/produto.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CriarProdutoComponent } from 'src/app/components/produto/criar-produto/criar-produto.component';
 
 const routes: Routes = [
-  {path: 'produtos', component: ProdutoComponent}
+  {path: 'produtos', component: ProdutoComponent},
+  {path: 'produtos/criar', component: CriarProdutoComponent}
 ];
 
 @NgModule({
